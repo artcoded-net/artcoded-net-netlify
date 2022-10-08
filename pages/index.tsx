4dfdced5959e8d1c5f6ea379150c80c1f8d295a8
Artcoded <dev@artcoded.net>
2022-10-09 01:01:42 +0200

    change casing config cms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
+import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
     <div>
       <NextSeo title={"Home Page"} description="" />
-      <main>ciao</main>
+      <main>
+        <Hero callToAction="Learn full-stack Javascript" imageSrc="" />
+      </main>
     </div>
   );
 };
